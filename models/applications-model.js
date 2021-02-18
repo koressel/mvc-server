2d8176358681c00a02213bc1e88bded9ce3c3ed0
koressel <koressel.dev@gmail.com>
2021-02-18 09:28:03 -0600

    set config values for database

--- models/applications-model.js
+++ models/applications-model.js
@@ -1,22 +1,45 @@
+// DATABASE_URL = postgresql-infinite-40591 
+const { Client } = require('pg');
+
 module.exports = {
     getApplications: function() {
-        var data = [
-            {
-                position: 'Developer',
-                company: 'Google',
-                date: '02/28/2021'
-            },
-            {
-                position: 'Web Developer',
-                company: 'Facebook',
-                date: '02/28/2021'
-            },
-            {
-                position: 'Tech Lead',
-                company: 'Spotify',
-                date: '02/28/2020'
-            },
-        ];
+        const client = new Client({
+            connectionString: 'postgresql-infinite-40591',
+            ssl: {
+                rejectUnauthorized: false
+            }
+            });
+
+        client.connect();
+
+        let data = [];
+        
+        client.query('SELECT * FROM applications;', (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+            // console.log(JSON.stringify(row));
+            data.push(JSON.stringify(row))
+        }
+        client.end();
+        });
+
+        // var data = [
+        //     {
+        //         position: 'Developer',
+        //         company: 'Google',
+        //         date: '02/28/2021'
+        //     },
+        //     {
+        //         position: 'Web Developer',
+        //         company: 'Facebook',
+        //         date: '02/28/2021'
+        //     },
+        //     {
+        //         position: 'Tech Lead',
+        //         company: 'Spotify',
+        //         date: '02/28/2020'
+        //     },
+        // ];
         return data;
     }
 }

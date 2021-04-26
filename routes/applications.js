277aa2d6fd8199019bcae69ed3328edae8856c80
koressel <koressel.dev@gmail.com>
2021-04-26 09:12:59 -0500

    remove fileReader

--- routes/applications.js
+++ routes/applications.js
@@ -1,4 +1,5 @@
 var express = require('express');
+fs = require('fs');
 var applicationsController = require('../controllers/applications-controller');
 var router = express.Router();
 
@@ -15,5 +16,26 @@ router.post('/delete', (req,res) => {
 router.post('/update', (req,res) => {
     applicationsController.editApplication(req,res);
 });
+router.get('/files/:fileName', (req,res) => {
+    let stream = fs.createReadStream('public/assets/testResume.pdf');
+    let fileName = req.params.fileName;
+    fileName = encodeURIComponent(fileName);
+
+    res.setHeader('Content-disposition', 'inline; filename="' + fileName + '"');
+    res.contentType("application/pdf");
+
+    stream.pipe(res);
+
+    // fs.readFile(filePath , function (err,data){
+    //     res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
+    //     res.contentType("application/pdf");
+    //     res.send(data);
+    // });
+
+    // res.sendFile('testResume.pdf', {root: './public/assets/'});
+    // res.download('public/assets/testResume.pdf', 'resume.pdf');
+
+});
+
 
 module.exports = router;
